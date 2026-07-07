140d2bc4057f1c412478ca1c94d470a0c675dd6b
Poojabhoye <poojabhoye177@gmail.com>
2026-07-07 11:20:59 +0530

    Create UserNotFoundAdvice.java

---
+++ UserNotFoundAdvice.java
@@ -0,0 +1,24 @@
+package com.demo.user.frontend.exception;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ControllerAdvice
+public class UserNotFoundAdvice {
+
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(UserNotFoundException.class)
+    public Map<String, String> exceptionHandler(UserNotFoundException exception) {
+
+        Map<String, String> errorMap = new HashMap<>();
+
+        errorMap.put("errorMessage", exception.getMessage());
+
+        return errorMap;
+    }
+}
